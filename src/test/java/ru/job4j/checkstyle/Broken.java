28223c733f8b19307e7add13db916a41a5640f52
Trush22 <89226079875@mail.ru>
2023-02-05 17:29:29 +0500

    Правила Checkstyle [#504981]

---
+++ src/test/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,25 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final String NEWVALUE = "";
+
+    public String surname;
+
+    private int sizeOfEmpty = 10;
+
+    void echo() { }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f) {
+    }
+
+    String name;
+
+    Broken() { }
+
+}

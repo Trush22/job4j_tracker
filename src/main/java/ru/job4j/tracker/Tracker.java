85a461188d6038cc68a11404adfd3c45a7c17d5b
Trush22 <89226079875@mail.ru>
2023-03-01 00:19:51 +0500

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -20,6 +22,40 @@ public class Tracker {
                 break;
             }
         }
+        return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[size];
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
+            if (item != null) {
+                rsl[i] = item;
+                size++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, size);
+        return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        for (int i = 0; i < size; i++) {
+            Item name = items[i];
+            if (key.equals(name.getName())) {
+                rsl[i] = name;
+            }
+        }
+        Item[] rsl2 = new Item[size];
+        int size2 = 0;
+        for (int i = 0; i < size; i++) {
+            Item item = rsl[i];
+            if (item != null) {
+                rsl2[size2] = item;
+                size2++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl2, size2);
         return rsl;
     }
 }

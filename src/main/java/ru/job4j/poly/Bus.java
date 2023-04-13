b1d64b247b8aab584ed427bff1bfc0a86b398be5
Trush22 <89226079875@mail.ru>
2023-04-13 22:35:43 +0500

    4.2. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,17 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+    }
+
+    @Override
+    public void passengers(int quantity) {
+    }
+
+    @Override
+    public double refill(double fuel) {
+        return 0;
+    }
+}

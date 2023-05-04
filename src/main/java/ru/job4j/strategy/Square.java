5861f7a0f2c9f9c37fc14d6c7abc0bbc1e6bbb51
Trush22 <89226079875@mail.ru>
2023-05-04 23:52:20 +0500

    11.0. Шаблон проектирования - Стратегия [#786]

---
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -0,0 +1,10 @@
+package ru.job4j.strategy;
+
+public class Square implements Shape {
+    public String draw() {
+        String ln = System.lineSeparator();
+        return    " _____ " + ln
+                + "|     |" + ln
+                + "|_____|" + ln;
+    }
+}
